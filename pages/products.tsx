00e5d44251293b3d1e556fa9deec8727032f58d8
jintae-1224 <kab7210@naver.com>
2022-12-28 02:42:38 +0900

    Image Carousel

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,4 +1,8 @@
-import ImageGallery from 'react-image-gallery'
+// import ImageGallery from 'react-image-gallery'
+
+import Image from 'next/image'
+import Carousel from 'nuka-carousel/lib/carousel'
+import { useState } from 'react'
 
 const images = [
   {
@@ -29,8 +33,57 @@ const images = [
     original: 'https://picsum.photos/id/1019/1000/600/',
     thumbnail: 'https://picsum.photos/id/1019/250/150/',
   },
+  {
+    original: 'https://picsum.photos/id/188/1000/600/',
+    thumbnail: 'https://picsum.photos/id/188/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/111/1000/600/',
+    thumbnail: 'https://picsum.photos/id/111/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/122/1000/600/',
+    thumbnail: 'https://picsum.photos/id/122/250/150/',
+  },
 ]
 
 export default function Products() {
-  return <ImageGallery items={images} />
+  // return <ImageGallery items={images} />
+  const [index, setIndex] = useState<number>(0)
+  return (
+    <>
+      <Carousel
+        withoutControls
+        wrapAround
+        autoplay
+        speed={10}
+        slideIndex={index}
+      >
+        {images.map((item) => (
+          <Image
+            key={item.original}
+            src={item.original}
+            alt="image"
+            width={1000}
+            height={600}
+            layout="responsive"
+          />
+        ))}
+      </Carousel>
+      <div style={{ display: 'flex' }}>
+        {images.map((item, idx) => (
+          <div key={idx} onClick={() => setIndex(idx)}>
+            <Image
+              key={item.original}
+              src={item.original}
+              alt="image"
+              width={100}
+              height={60}
+              layout="responsive"
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  )
 }
