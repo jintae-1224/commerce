8a68fd40436e0540d5ca404040acc152a7b0fd91
JinTae <kab7210@naver.com>
2022-12-27 18:01:38 +0900

    notion Api --save

---
+++ pages/api/add-item.ts
@@ -0,0 +1,49 @@
+import { Client } from '@notionhq/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const notion = new Client({
+  auth: 'your secret Key',
+})
+
+const databaseId = 'your database ID'
+
+async function addItem(name: string) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: [
+          {
+            text: {
+              content: name,
+            },
+          },
+        ],
+      },
+    })
+    console.log(response)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+type Data = {
+  message: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { name } = req.query
+
+  if (name === null) {
+    return res.status(400).json({ message: 'No Name' })
+  }
+  try {
+    await addItem(String(name))
+    res.status(200).json({ message: `Success ${name} add` })
+  } catch (error) {
+    return res.status(400).json({ message: 'Fail To add' })
+  }
+}
