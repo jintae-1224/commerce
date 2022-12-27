91dd1889d70b973ba24055d07482c41a9ff22502
jintae-1224 <kab7210@naver.com>
2022-12-28 01:32:05 +0900

    input tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,7 +60,12 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
-          <input ref={inputRef} type="text" placeholder="name" />
+          <input
+            className='"placeholder:italic placeholder:text-slate-400 block bg-white w-96 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"'
+            ref={inputRef}
+            type="text"
+            placeholder="name"
+          />
           <button onClick={handleClick}>Add Jacket</button>
           <div>
             <p>Product List</p>
