08ba2223030cf95d2643bba37ac171d172b23acc
JinTae <kab7210@naver.com>
2022-12-28 13:24:23 +0900

    Head meta OG 추가

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,5 +1,6 @@
 // import ImageGallery from 'react-image-gallery'
 
+import Head from 'next/head'
 import Image from 'next/image'
 import Carousel from 'nuka-carousel/lib/carousel'
 import { useState } from 'react'
@@ -52,6 +53,25 @@ export default function Products() {
   const [index, setIndex] = useState<number>(0)
   return (
     <>
+      <Head>
+        <meta
+          property="og:url"
+          content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
+        />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:title"
+          content="When Great Minds Don’t Think Alike"
+        />
+        <meta
+          property="og:description"
+          content="How much does culture influence creative thinking?"
+        />
+        <meta
+          property="og:image"
+          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
+        />
+      </Head>
       <Carousel
         withoutControls
         wrapAround
