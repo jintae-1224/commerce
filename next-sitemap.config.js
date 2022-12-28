5fde6f8d662a479923627f3e81d0367d0745420b
JinTae <kab7210@naver.com>
2022-12-28 13:21:35 +0900

    yarn add -D next-sitemap && siteMap 생성

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://next-js-blog-omega-wheat.vercel.app/',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
